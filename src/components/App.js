d7f9f296fecf048bf59014d426b631dc48d1fc7c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 19:34:28 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React, { Component, useState } from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
+import "../styles/App.css"
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false);
-  
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div id="main">
-   
-      {isClicked && (
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-        </p>
-      )}
-        
-      <button id="click" onClick={() => {setIsClicked(true);}}>
+      <p id="para" className={isVisible ? 'show' : 'hide'}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
+      <button id="click" onClick={handleClick}>
         Click me
       </button>
     </div>
   );
 }
 
-export default App
+export default App;
